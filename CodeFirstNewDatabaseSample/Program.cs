72eee6448bd48f2685b3fcb1610edd4254155704
Marilynflo <107453088+Marilynflo@users.noreply.github.com>
2023-01-15 17:04:02 -0800

    Add files via upload

---
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class Program
+{
+    public static void Main(object FiddleHelper)
+    {
+        using (var ctx = new SchoolContext())
+        {
+            var stud = new Student() { StudentName = "Bill" };
+
+            ctx.Students.Add(stud);
+            ctx.SaveChanges();
+
+            Console.Write("Student saved successfully!");
+
+            // The following displays Student table data - dotnetfiddle.net specific code
+            //FiddleHelper.WriteTable("Student", ctx.Students.ToList());
+        }
+    }
+}
+
