ffcb6ac1970c94d038cb5ba4eb2510d985987858
Marilynflo <107453088+Marilynflo@users.noreply.github.com>
2023-01-15 23:28:42 -0800

    Add files via upload

---
+++ MethodClass/Integers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ClassMethod
+{
+    public class MathOp
+    {
+        public void MathMethodFirst(int num1, int num2)  //creating a class that takes two integers
+        {
+            int result = num1 / 2;
+            int result2 = num2 / 2;
+            Console.WriteLine("Your number divided by 2 = " + result2);
+
+        }
+
+       
+    }
+}
---
+++ MethodClass/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num1 = 10;
+
+            
+
+            MathOp FirstOp = new MathOp(); //instantiating the class
+            FirstOp.MathMethodFirst (int num1, int num2);
+
+            
+
+            Console.WriteLine(num2); //writing in the console the second parameter
+            Console.WriteLine("Passing 9 as an integer");
+
+           
+
+
+            Console.ReadLine();
+        }
+    }
+}
